baa0fb4dea0aa428cfb09fa58b8103fdc657cb06
Micolash VI <mharakati@unomaha.edu>
2024-04-07 15:43:16 -0500

    DB_configuration

--- website/website/settings.py
+++ website/website/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),  # Default MySQL port
     }
 }
+
 
 
 # Password validation
