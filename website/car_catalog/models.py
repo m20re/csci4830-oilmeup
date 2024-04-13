aef7c7afad103eb51401cc559f25044e3db5c572
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-04-13 13:03:37 -0500

    updated detail.html, resize images when uploaded

--- website/car_catalog/models.py
+++ website/car_catalog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.urls import reverse
 from django.conf import settings
 from django.db.models.functions import Lower
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import io
 
 
 class Car(models.Model):
@@ -17,7 +20,6 @@ class Car(models.Model):
         ('automatic', 'Automatic'),
         ('manual', 'Manual'),
     }
-
     transmission = models.CharField(max_length=50, choices=TRANSMISSION_CHOICES, default='automatic')
 
     FUEL_CHOICES = {
@@ -25,10 +27,37 @@ class Car(models.Model):
         ('diesel', 'Diesel'),
         ('electric', 'Electric')
     }
-
     fuel_type = models.CharField(max_length=50, choices=FUEL_CHOICES, default='gasoline')
 
     image = models.ImageField(upload_to='cars_images', blank=True, null=True)
+
+    # resizes the image
+    def save(self, *args, **kwargs):
+        # If an image exists, resize it
+        if self.image:
+            # open the image
+            im = Image.open(self.image)
+
+            # Convert to RGB to avoid errors
+            if im.mode != 'RGB':
+                im = im.convert('RGB')
+            
+            # Resize the image
+            im.thumbnail((600, 400), Image.Resampling.LANCZOS)
+
+            # Move the image into memory
+            output = io.BytesIO()
+            im.save(output, format='JPEG', quality=100)
+            output.seek(0)
+
+            # Overwrite the old image
+            self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg', output.getbuffer().nbytes, None)
+        
+        # Use django's ORM save to save to the database
+        super(Car, self).save(*args, **kwargs)
+
+
+
 
 
     class Meta:
