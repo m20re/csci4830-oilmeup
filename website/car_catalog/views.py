04f52ad084b73e5d3255f92bd1dcc87d76d18454
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-04-13 00:00:31 -0500

    modified car website homepage

--- website/car_catalog/views.py
+++ website/car_catalog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Car
 
 def index(request):
     """View function for home page of the site"""
+    # gets the amount of cars
+    num_cars = Car.objects.all().count()
 
     context = {
-        
+        'num_cars' : num_cars,
     }
 
     return render(request, 'index.html', context=context)
