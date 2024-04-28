ecd66247c1f3a1beb305c9138d2b633770f5a452
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-04-28 17:09:31 -0500

    modified signup.html to display errors

--- website/car_catalog/forms.py
+++ website/car_catalog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import UserProfile
@@ -9,6 +10,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('budget',)
+
+    # raises validation error if the username already exists within database
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken. ")
+        return username
 
     def save(self, commit=True):
         user = super().save(commit=False)
